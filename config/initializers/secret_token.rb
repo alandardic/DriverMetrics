4f350cced322fc509cdeaccaa177d50d1f983125
alan dardic <alandardic9@gmail.com>
2014-03-28 15:07:58 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MatricsProject::Application.config.secret_key_base = 'ff061aa4c91823577b393aa617cc95fe0dd032822034e18ac74eed657ba5b1951c035faee55babd9c7db702fb205ed63cdf8d443991c74cc2630bf7ea4ed1921'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MatricsProject::Application.config.session_store :cookie_store, key: '_matrics_project_session'
